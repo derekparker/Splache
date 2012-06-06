caf796e47a554ef877edc44ba17e9e27ff08ad3a
Derek Parker <parkerderek86@gmail.com>
2012-06-05 21:13:41 -0500

    Added Socket.cpp

---
+++ src/Socket.cpp
@@ -0,0 +1,11 @@
+//
+//  Socket.cpp
+//  
+//
+//  Created by Derek Parker and Kyle Nusbaum on 6/5/12.
+//  Copyright (c) 2012 Splache All rights reserved.
+//
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
