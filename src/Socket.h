f2e54432d15454871146bf2d1946b46983a9929e
Derek Parker <parkerderek86@gmail.com>
2012-06-05 21:00:48 -0500

    Added Socket.h and updated documentation

---
+++ src/Socket.h
@@ -0,0 +1,17 @@
+//
+//  Socket.h
+//  
+//
+//  Created by Derek Parker and Kyle Nusbaum on 6/5/12.
+//  Copyright (c) 2012 Splache. All rights reserved.
+//
+
+#ifndef _Socket_h
+#define _Socket_h
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+
+#endif
