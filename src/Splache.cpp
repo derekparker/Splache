fbe269fd470c7caac678e03a4f3ee3759f1d8e9d
Derek Parker <parkerderek86@gmail.com>
2012-06-05 20:21:21 -0500

    Setting up the main Spache files and initial main function

---
+++ src/Splache.cpp
@@ -0,0 +1,20 @@
+//
+//  Splache.cpp
+//  
+//
+//  Created by Derek Parker on 6/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+#include "Splache.h"
+
+int main(int argc, char* argv[]) {
+    
+    /**
+     * Let's get the ball rolling!
+     */
+    
+    return 0;
+}
---
+++ src/Splache.h
@@ -0,0 +1,17 @@
+//
+//  Splache.h
+//  
+//
+//  Created by Derek Parker and Kyle Nusbaum.
+//  Copyright (c) 2012 Splache. All rights reserved.
+//
+
+#ifndef _Splache_h
+#define _Splache_h
+
+/**
+ * A start to the header file.
+ * Just committing to get the ball rolling!
+ */
+
+#endif
