87efbe2413ee7a7f0fcee77f62e1123ea534bdcc
Dparker1990 <parkerderek86@gmail.com>
2012-06-16 01:07:15 -0500

    Updating meta info

--- test/log/Log.cpp
+++ test/log/Log.cpp
@@ -2,8 +2,8 @@
 //  Log.cpp
 //  
 //
-//  Created by Derek Parker on 6/16/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Created by Derek Parker and Kyle Nusbaum on 6/16/12.
+//  Copyright (c) 2012 Splache. All rights reserved.
 //
 
 #include <iostream>
