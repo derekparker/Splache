89f6b9068600c54c248d697797b9d8bdcce1bf30
Dparker1990 <parkerderek86@gmail.com>
2012-06-18 19:43:32 -0500

    Moving some files around

---
+++ src/Log.cpp
@@ -0,0 +1,35 @@
+/**  
+ *  Log.cpp
+ *
+ *  Created by Derek Parker and Kyle Nusbaum.
+ */
+
+#include <iostream>
+#include "Log.h"
+
+Log::Log(char* logfile)
+{
+    m_strea.open(logfile);
+}
+
+Log::logException(char* exceptionThrown)
+{
+    m_stream << getDateTime() << exceptionThrown << endl;
+}
+
+Log::getDateTime()
+{
+    time_t now = time(0);
+    struct tm t;
+    char buf[80];
+    tstruct = *localtime(&now);
+    
+    strftime(buf, sizeof(buf), "%Y-%m-%d.%X", &t);
+    
+    return buf;
+}
+
+Log::~Log()
+{
+    m_stream.close();
+}
